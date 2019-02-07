6751934410aa07b23f86b422fbca42a99c62c367
Greg Niemann <greg.niemann@willowtreeapps.com>
2019-02-07 13:07:35 -0500

    Refactor lights into a manager class.

---
+++ src/light_manager.h
@@ -0,0 +1,138 @@
+//
+// Created by Greg Niemann on 2019-02-07.
+//
+
+#ifndef BUILD_MONITOR_LIGHTMANAGER_H
+#define BUILD_MONITOR_LIGHTMANAGER_H
+
+#include <Arduino.h>
+#include <array>
+#include <algorithm>
+#include "led.h"
+#include "config.h"
+
+using Pin = uint8_t;
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+class LightManager {
+public:
+    void off();
+    void failure();
+    void update(Configuration *config);
+    void setLights() const;
+    void step();
+private:
+    std::array<LEDPtr, N> lights;
+
+    void setAll(LEDPtr led);
+};
+
+Color colorForStatus(const Status &status) {
+    switch (status) {
+        case Status::SUCCEEDED:
+            return GREEN;
+        case Status::FAILED:
+            return RED;
+        case Status::QUEUED:
+        case Status::IN_PROGRESS:
+            return BLUE;
+        case Status::PENDING_APPROVAL:
+            return MAGENTA;
+        default:
+            return OFF;
+    }
+}
+
+std::shared_ptr<LED> ledForStage(const Stage &stage) {
+    auto primaryColor = colorForStatus(stage.getStatus());
+    switch (stage.getStatus()) {
+        case Status::SUCCEEDED:
+            return LEDPtr(new InitiallyBlinkingLED(primaryColor, 20, 2, 1));
+        case Status::FAILED:
+            return LEDPtr(new InitiallyBlinkingLED(primaryColor, 20, 1, 1));
+        case Status::IN_PROGRESS:
+        case Status::QUEUED:
+            return LEDPtr(new AlternatingLED(primaryColor, colorForStatus(stage.getPrevStatus()), 4, 4));
+        case Status::PENDING_APPROVAL:
+            return LEDPtr(new LED(primaryColor));
+        default:
+            return LEDPtr(new LED(OFF));
+    }
+}
+
+uint8_t colorToByte(const Color &c) {
+    uint8_t output = 0;
+
+    if (c.blue != 0) {
+        output++;
+    }
+    output = output<<1;
+    if (c.green != 0) {
+        output++;
+    }
+    output = output<<1;
+    if (c.red != 0) {
+        output++;
+    }
+
+    return output;
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::setAll(std::shared_ptr<LED> led) {
+    std::fill(lights.begin(), lights.end(), led);
+    setLights();
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::off() {
+    LEDPtr off(new LED(OFF));
+    setAll(off);
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::failure() {
+    LEDPtr failure(new LED(RED));
+    setAll(failure);
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::update(Configuration *config) {
+    auto light = lights.begin();
+    auto stage = config->begin();
+
+    while (light != lights.end() && stage != config->end()) {
+        if (stage->isChanged() || *light == nullptr) {
+            *light = ledForStage(*stage);
+        }
+
+        ++light;
+        ++stage;
+    }
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::setLights() const {
+    uint64_t output = 0;
+
+    // need to output in reverse order, so that the first light foes in the first position
+    for (auto iter = lights.rbegin(); iter != lights.rend(); ++iter) {
+        output = (output<<3) + colorToByte((*iter)->getColor());
+    }
+
+    digitalWrite(LATCH, LOW);
+    for (int i = sizeof(uint64_t) * 7; i >= 0; i-=8) {
+        uint8_t shift = (output >> i) & 0xFF;
+        shiftOut(DATA, CLOCK, MSBFIRST, ~shift);
+    }
+    digitalWrite(LATCH, HIGH);
+}
+
+template <Pin DATA, Pin CLOCK, Pin LATCH, std::size_t N>
+void LightManager<DATA, CLOCK, LATCH, N>::step() {
+    for (auto light: lights) {
+        light->step();
+    }
+}
+
+#endif //BUILD_MONITOR_LIGHTMANAGER_H
--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,13 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <vector>
-#include <array>
 #include <memory>
-#include <unordered_map>
 #include "color.h"
 #include "led.h"
 #include "status_client.h"
 #include "status.h"
 #include "config.h"
+#include "light_manager.h"
 
 const char* ssid = "Bodhi";
 const char* password = "wtaguest";
@@ -26,7 +24,6 @@ using Pin = uint8_t;
 using Hz = int;
 
 constexpr std::size_t ledCNT = 9;
-using LEDArray = std::array<LEDPtr, ledCNT>;
 
 const Pin DATA = D5;
 const Pin CLOCK = D6;
@@ -34,12 +31,9 @@ const Pin LATCH = D7;
 
 const Hz RATE = 5;
 
-LEDArray lights;
 auto config = std::make_shared<Configuration>();
+auto lights = LightManager<DATA, CLOCK, LATCH, ledCNT>();
 StatusClient client(status_url, fingerprint, authorization);
-
-LEDPtr failure(new LED(RED));
-LEDPtr off(new LED(OFF));
 
 void setupWifi() {
     WiFi.begin(ssid, password);
@@ -56,49 +50,6 @@ void setupWifi() {
     Serial.println(WiFi.localIP());
 }
 
-uint8_t colorToByte(const Color &c) {
-    uint8_t output = 0;
-
-    if (c.blue != 0) {
-        output++;
-    }
-    output = output<<1;
-    if (c.green != 0) {
-        output++;
-    }
-    output = output<<1;
-    if (c.red != 0) {
-        output++;
-    }
-
-    return output;
-}
-
-template <std::size_t N>
-uint64_t convertColors(const std::array<LEDPtr, N> &leds) {
-    uint64_t output = 0;
-    // need to output in reverse order, so that the first light goes in thh first position
-    for (auto iter = leds.rbegin(); iter != leds.rend(); ++iter) {
-        output = (output<<3) + colorToByte((*iter)->getColor());
-    }
-    return output;
-}
-
-void shiftOut(uint64_t output) {
-    digitalWrite(LATCH, LOW);
-
-    for (int i = sizeof(uint64_t) * 7; i >= 0; i-=8) {
-        uint8_t shift = (output >> i) & 0xFF;
-//        Serial.println(shift, BIN);
-        shiftOut(DATA, CLOCK, MSBFIRST, ~shift);
-    }
-    digitalWrite(LATCH, HIGH);
-}
-
-void setLights(const LEDArray &lights) {
-    shiftOut(convertColors(lights));
-}
-
 // the setup function runs once when you press reset or power the board
 void setup() {
     // initialize digital pin LED_BUILTIN as an output.
@@ -108,7 +59,7 @@ void setup() {
     pinMode(LATCH, OUTPUT);
 
     // turn all LEDs off
-    shiftOut(0);
+    lights.off();
 
     digitalWrite(LED_BUILTIN, HIGH);
 
@@ -118,39 +69,6 @@ void setup() {
     digitalWrite(LED_BUILTIN, LOW);
 }
 
-Color colorForStatus(const Status &status) {
-    switch (status) {
-        case Status::SUCCEEDED:
-            return GREEN;
-        case Status::FAILED:
-            return RED;
-        case Status::QUEUED:
-        case Status::IN_PROGRESS:
-            return BLUE;
-        case Status::PENDING_APPROVAL:
-            return MAGENTA;
-        default:
-            return OFF;
-    }
-}
-
-std::shared_ptr<LED> ledForStage(const Stage &stage) {
-    auto primaryColor = colorForStatus(stage.getStatus());
-    switch (stage.getStatus()) {
-        case Status::SUCCEEDED:
-            return LEDPtr(new InitiallyBlinkingLED(primaryColor, 20, 2, 1));
-        case Status::FAILED:
-            return LEDPtr(new InitiallyBlinkingLED(primaryColor, 20, 1, 1));
-        case Status::IN_PROGRESS:
-        case Status::QUEUED:
-            return LEDPtr(new AlternatingLED(primaryColor, colorForStatus(stage.getPrevStatus()), 4, 4));
-        case Status::PENDING_APPROVAL:
-            return LEDPtr(new LED(primaryColor));
-        default:
-            return off;
-    }
-}
-
 constexpr int iterations = RATE * 30;
 // the loop function runs over and over again forever
 void loop() {
@@ -158,32 +76,16 @@ void loop() {
 
     Serial.println(resp);
     if (resp < 200 || resp >= 400) {
-        for (auto light: lights) {
-            light = failure;
-        }
+        lights.failure();
     } else if (resp != 304) {
         auto statuses = parse_json(client.getStream());
         config->update(statuses);
-
-        auto light = lights.begin();
-        auto stage = config->begin();
-
-        while (light != lights.end() && stage != config->end()) {
-            if (stage->isChanged() || *light == nullptr) {
-                *light = ledForStage(*stage);
-            }
-
-            ++light;
-            ++stage;
-        }
+        lights.update(config.get());
     }
 
     for (int step = 0; step < iterations; ++step) {
-        setLights(lights);
-
-        for (auto light : lights) {
-            light->step();
-        }
+        lights.setLights();
+        lights.step();
 
         delay(1000 / RATE);
     }
